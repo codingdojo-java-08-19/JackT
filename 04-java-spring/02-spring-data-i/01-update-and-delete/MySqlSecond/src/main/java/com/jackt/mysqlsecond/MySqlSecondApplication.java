408fb5bb9c63c94c0c9dba3a22c14c6b407e7c00
jackt112212 <jackt12390@hotmail.com>
2019-08-20 13:19:01 -0700

    assignment update

---
+++ 04-java-spring/02-spring-data-i/01-update-and-delete/MySqlSecond/src/main/java/com/jackt/mysqlsecond/MySqlSecondApplication.java
@@ -0,0 +1,13 @@
+package com.jackt.mysqlsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySqlSecondApplication.class, args);
+	}
+
+}
